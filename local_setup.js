2cba320c790e337d1de451cf7ebe082c05d19721
phil jones <interstar@gmail.com>
2014-01-17 23:26:17 -0200

    adding setup.js and local_setup.js as a place to refactor variance between versions, everything else should eventually be common

---
+++ local_setup.js
@@ -0,0 +1,5 @@
+// local_setup.js 
+// ideally, all customizations for version live in here
+
+// This is the single-page localStorage version
+
---
+++ static/local_setup.js
@@ -0,0 +1,5 @@
+// local_setup.js 
+// ideally, all customizations for version live in here
+
+// THIS IS THE OWLServe setup file
+
