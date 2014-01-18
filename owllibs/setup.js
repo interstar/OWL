5d289186f195de897e4b5b249baacd656b879ce3
phil jones <interstar@gmail.com>
2014-01-18 00:12:58 -0200

    refactoring to setup.js, local_setup.js

--- owllibs/setup.js
+++ owllibs/setup.js
@@ -6,3 +6,37 @@ function markdownExport() {
     });
 }
 
+function setupEvents () {
+
+	$("#idMenuProductName").text (appConsts.productname);
+	$("#idProductVersion").text ("v" + appConsts.version);
+
+	//init menu keystrokes
+	if (navigator.platform.toLowerCase ().substr (0, 3) == "mac") {
+		cmdKeyPrefix = "&#8984;";
+	}
+	$("#idMenubar .dropdown-menu li").each (function () {
+		var li = $(this);
+		var liContent = li.html ();
+		liContent = liContent.replace ("Cmd-", cmdKeyPrefix);
+		li.html (liContent);
+	});
+
+
+	$("#goButton").click(function(event) { goButton(); });
+	$("#backButton").click(function(event) { backButton();  });
+	$("#forwardButton").click(function(event) { forwardButton();  });
+
+	$("#makeLinkButton").click(function(event) { makeLink(); });				
+	
+	$("#upButton").click(function(event)   { opReorg (up, 1); });
+	$("#downButton").click(function(event) { opReorg (down, 1); });
+	$("#leftButton").click(function(event)   { opReorg (left, 1); });
+	$("#rightButton").click(function(event)   { opReorg (right, 1); });
+	
+	
+	$("#forceSaveButton").click(function(event) { savePageNow(); });			
+	$("#markdownExportButton").click(function(event) { markdownExport(); });
+
+}
+
--- static/local_setup.js
+++ static/local_setup.js
@@ -1,5 +1,5 @@
 // local_setup.js 
 // ideally, all customizations for version live in here
 
-// THIS IS THE OWLServe setup file
+// This is the OWLServe setup file
 
